6b77b78e45c465ee4fbcae01817ac16383fb9688
simon040102 <simon040102@gmail.com>
2022-09-22 16:35:51 +0800

    新增MAC資訊

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ var Url = require('../models/urlModel');
 var parser = require('ua-parser-js');
 const clickedInf = require('../models/clickedInfModel');
 const appError = require('../service/appError');
+const os = require('os');
 
 /* GET home page. */
 router.get('/:id', async function (req, res, next) {
   const url = req.params.id;
   var ua = parser(req.headers['user-agent']);
   const findUrl = await Url.find({ shortUrl: url });
-  // console.log(ua);
+  var mac_ip =  os.networkInterfaces()
+  const mac = mac_ip.en0[0].mac;
   const clicked = {
     UserBowse: ua.browser.name,
     UserInform: ua.ua,
@@ -21,17 +23,17 @@ router.get('/:id', async function (req, res, next) {
   }
   if (url.length == 6) {
    await clickedInf.updateOne(
-      { shortUrl: url },
-      {
-        $addToSet: {
-          clicked: {
-            UserBowse: ua.browser.name,
-            UserInform: ua.ua,
-            UserSystem: ua.os.name,
-          },
-        },
-      }
-    );
+     { shortUrl: url },
+     {
+       $addToSet: {
+         clicked: {
+           UserBowse: ua.browser.name,
+           UserInform: mac,
+           UserSystem: ua.os.name,
+         },
+       },
+     }
+   );
     const originUrl = findUrl[0].url;
     res.redirect(`${originUrl}`);
     
