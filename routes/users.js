2f49d5df7f45723014722203f0889f8008ce5a9b
simon040102 <simon040102@gmail.com>
2022-09-26 12:36:09 +0800

    新增#

--- routes/users.js
+++ routes/users.js
@@ -132,7 +132,7 @@ router.post(
         from: process.env.MAIL_FROM,
         to: email,
         subject: 'BlueCircle 忘記密碼驗證信',
-        html: `<p>請點擊連擊修改密碼<p/><a href="http://127.0.0.1:5173/changepassword/${token}">驗證連結</a>`,
+        html: `<p>請點擊連擊修改密碼<p/><a href="http://127.0.0.1:5173/#/changepassword/${token}">驗證連結</a>`,
       })
       .then((info) => {
         console.log({ info });
